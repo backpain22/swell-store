a7527ef0ce7c0678d51f2543a1ee12dd97b415ec
backpain22 <97351459+backpain22@users.noreply.github.com>
2022-09-19 17:37:48 -0700

    Update _id.vue

--- pages/account/orders/_id.vue
+++ pages/account/orders/_id.vue
@@ -497,8 +497,8 @@ export default {
   methods: {
     getUrl(prodName) {
       AWS.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
         region: 'us-west-1',
         signatureVersion: 'v4',
       });
