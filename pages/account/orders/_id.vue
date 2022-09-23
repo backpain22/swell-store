63ac91b35b14e2fe44b38dafdc30b361dc687729
backpain22 <97351459+backpain22@users.noreply.github.com>
2022-09-23 11:18:56 -0700

    Update _id.vue

--- pages/account/orders/_id.vue
+++ pages/account/orders/_id.vue
@@ -513,11 +513,10 @@ export default {
        if (xhr.status !== 200) { // analyze HTTP status of the response
           alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
         } else {
-            console.log("response" + xhr.response);
-            console.log("response text" + xhr.responseText)
+
             
-            myurl = xhr.response.url;
-            console.log(myurl);
+            myurl = xhr.responseText;
+   
         }
       };
 
