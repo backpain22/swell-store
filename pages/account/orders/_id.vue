8ae04751fb71d7b077ebc82a18d41c577ddd10e8
backpain22 <97351459+backpain22@users.noreply.github.com>
2022-09-24 10:06:07 -0700

    Update _id.vue

--- pages/account/orders/_id.vue
+++ pages/account/orders/_id.vue
@@ -494,45 +494,17 @@ export default {
     },
   },
   methods: {
-    getUrl() {
-      const data = {};
-      let myurl = ""
-      const funcurl = process.env.MY_LAMBDA_URL;
-      data.id = "The Drip Kit";
-      data.type = ".zip";
-      
-      let xhr = new XMLHttpRequest();
+  async function apiCall(parameter) {
+    const url = `/netlify/functions/geturl.js`;
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+}
 
-// 2. Configure it: GET-request for the URL /article/.../load
-xhr.open('GET', funcurl);
-
-// 3. Send the request over the network
-xhr.send(JSON.stringify(data));
-
-// 4. This will be called after the response is received
-xhr.onload = function() {
-  if (xhr.status != 200) { // analyze HTTP status of the response
-    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-  } else { // show the result
-    myurl = responseText;
-  }
-};
-
-xhr.onprogress = function(event) {
-  if (event.lengthComputable) {
-    alert(`Received ${event.loaded} of ${event.total} bytes`);
-  } else {
-    alert(`Received ${event.loaded} bytes`); // no Content-Length
-  }
-
-};
-
-xhr.onerror = function() {
-  alert("Request failed");
-};
-    
-      return myurl;
     },
-  },
 };
 </script>
