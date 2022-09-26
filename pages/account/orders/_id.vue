dee77516f40ce85a98a0752cebc8891b1ff1b8ac
backpain22 <97351459+backpain22@users.noreply.github.com>
2022-09-26 07:00:32 -0700

    Update _id.vue

--- pages/account/orders/_id.vue
+++ pages/account/orders/_id.vue
@@ -459,7 +459,7 @@ export default {
       let xhr = new XMLHttpRequest();
 
 // 2. Configure it: GET-request for the URL /article/.../load
-        xhr.open('GET', theurl);
+        xhr.open('GET', theurl, false);
 
 // 3. Send the request over the network
         xhr.send();
@@ -469,7 +469,7 @@ export default {
           if (xhr.status != 200) { // analyze HTTP status of the response
             alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
           } else { // show the result
-             myurl = xhr.response.url
+             myurl = xhr.response.Geturl
           };
        };
        return myurl
