46953589fc29a9eed0b73e4f564407162b05d5ff
backpain22 <97351459+backpain22@users.noreply.github.com>
2022-09-28 08:26:27 -0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -62,8 +62,8 @@ export default {
           body: true,
         },
         {
-          children: '!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','t2_sw7fik74', {"optOut":false,"useDecimalCurrencyValues":true,"aaid":"<AAID-HERE>","email":"<EMAIL-HERE>","idfa":"<IDFA-HERE>"});rdt('track', 'PageVisit');'
-        {
+          children: "!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','t2_sw7fik74', {"optOut":false,"useDecimalCurrencyValues":true,"aaid":"<AAID-HERE>","email":"<EMAIL-HERE>","idfa":"<IDFA-HERE>"});rdt('track', 'PageVisit');",
+        }
       ],
       link: [
         {
